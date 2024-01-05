75cc0d7783c3b1d58399c2fddb5e5eebef431377
Ravuthz <ravuthz@gmail.com>
2024-01-05 13:25:32 +0700

    Add tailwincss with postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
 import { createSignal } from "solid-js";
 import solidLogo from "./assets/solid.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = createSignal(0);
 
   return (
     <>
-      <div test-id="App">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
+      <div test-id="App" class="container mx-auto">
+        <div class="flex flex-col justify-center items-center mt-10">
+          <a class="py-4" href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} class="w-14 h-14" alt="Vite logo" />
+          </a>
+          <a class="py-4" href="https://solidjs.com" target="_blank">
+            <img src={solidLogo} class="w-14 h-14" alt="Solid logo" />
+          </a>
+        </div>
       </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div class="flex flex-col justify-center items-center">
+        <h1 class="text-3xl font-bold my-5">Vite + Solid</h1>
+        <button
+          class="g-blue-500 bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count()}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
     </>
   );
 }
